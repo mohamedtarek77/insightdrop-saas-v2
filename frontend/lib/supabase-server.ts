956fca3df1927f802ad8436dd0cd1c7fb6f7585a
mohamedtarek77 <mtarek7878@gmail.com>
2026-06-12 05:53:08 +0400

    04

--- frontend/lib/supabase-server.ts
+++ frontend/lib/supabase-server.ts
@@ -18,7 +18,7 @@ export async function createServerSupabaseClient() {
         },
         setAll(cookiesToSet) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
+            cookiesToSet.forEach(({ name, value, options }) : any =>
               cookieStore.set(name, value, options)
             );
           } catch {
